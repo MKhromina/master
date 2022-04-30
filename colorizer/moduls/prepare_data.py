f4907f75e2e8236b4ba794656467dd1e6ecfaf09
MKhromina <marina.mise.khromina@gmail.com>
2022-04-30 12:07:15 +0300

    refactor

--- colorizer/moduls/prepare_data.py
+++ colorizer/moduls/prepare_data.py
@@ -1,11 +1,12 @@
 import os
 import numpy as np
 from PIL import Image
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from keras.preprocessing.image import img_to_array, load_img
 from . import paths
+from .constants import SIZE
 
 
-def resize_img(img_name, size=(225, 225), raw_path=paths.RAW_DATA_PATH, final_path=paths.TEST_DATA_PATH):
+def resize_img(img_name, size=SIZE, raw_path=paths.RAW_DATA_PATH, final_path=paths.TEST_DATA_PATH):
     image = Image.open(os.path.join(raw_path, img_name))
     resized_image = image.resize(size)
     resized_image.save(os.path.join(final_path, f'resized_{img_name}'))
