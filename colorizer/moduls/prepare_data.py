42ad9cf5df8734318e12eae5e54d5d2a225a5bbd
MKhromina <marina.mise.khromina@gmail.com>
2022-05-03 13:46:49 +0300

    upd function

--- colorizer/moduls/prepare_data.py
+++ colorizer/moduls/prepare_data.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 from keras.preprocessing.image import img_to_array, load_img
 from . import paths
-from .constants import SIZE
+from .constants import SIZE, RGB_24
 
 
 def resize_img(img_name, size=SIZE, raw_path=paths.RAW_DATA_PATH, final_path=paths.TEST_DATA_PATH):
@@ -12,9 +12,11 @@ def resize_img(img_name, size=SIZE, raw_path=paths.RAW_DATA_PATH, final_path=pat
     resized_image.save(os.path.join(final_path, f'resized_{img_name}'))
     pass
 
-def imgs_to_np(x, pt):
+def imgs_to_np(pt):
     x = []
     for file in os.listdir(pt):
         x.append(img_to_array(load_img(os.path.join(pt, file))))
     x = np.array(x, dtype=float)
+    x /= RGB_24
+    return x
 
